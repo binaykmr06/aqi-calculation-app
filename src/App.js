13f2d5b4ddfab565537089a0677c40d416afe64a
Binay Kumar Panda <binaykmr06@gmail.com>
2022-01-08 18:44:24 +0530

    Added Error 404

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import ExtraPage from './components/ExtraPage';
+import Error404 from './components/Error404';
 
 export default class App extends React.Component {
   render() {
     return <BrowserRouter>
     <Routes>
-      <Route path="/" element={<HomePage />}/>
-        <Route path="/extra" element={<ExtraPage />} />
+        <Route exact={true} path="/" element={<HomePage />}/>
+        <Route exact={true} path="/extra" element={<ExtraPage />} />
+        <Route exact={true} path="/404" element={<Error404 />} />
     </Routes>
   </BrowserRouter>;
   }
---
+++ src/components/Error404.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Layout, Button } from 'antd';
+import NavBar from './NavBar';
+
+const { Content } = Layout;
+
+export default class Error404 extends React.Component {
+	render() {
+		return <Layout>
+			<NavBar activePage=""/>
+				<Content className='page-content error404-page-content'>
+                    <div className="error404-page center">
+                        <h1 className='text-center'>Awwwwww!!!</h1>
+                        <h2 className='text-center'>You Laded on wrong page !</h2>
+                    <Link className='center' to={'/'}><Button type="primary">Go to Home Page</Button></Link>
+                </div>
+			</Content>
+	</Layout>;
+	}
+}
